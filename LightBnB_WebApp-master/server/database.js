639b53253c2d1b701926242b2d18af6c9a79c357
neethu-ms <msneethu2989@gmail.com>
2020-03-05 19:15:34 +0000

    Fetch reservation data

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -64,7 +64,14 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool.query(`SELECT properties.*, reservations.*, AVG(property_reviews.rating) AS average_rating 
+FROM reservations JOIN properties ON reservations.property_id = properties.id 
+JOIN property_reviews ON property_reviews.property_id = properties.id 
+WHERE reservations.end_date < now()::date AND reservations.guest_id = $1
+GROUP BY properties.id, reservations.id
+ORDER BY reservations.start_date 
+LIMIT $2 ;`,[guest_id, limit]).then(res => res.rows);
+  
 };
 exports.getAllReservations = getAllReservations;
 
